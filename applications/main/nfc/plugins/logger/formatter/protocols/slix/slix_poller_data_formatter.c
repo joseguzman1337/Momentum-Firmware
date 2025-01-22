a3ec7aea9b09415587b2b3bdc19911f06b3f92e8
RebornedBrain <RebornedBrain@gmail.com>
2025-01-22 16:00:15 +0300

    State names added for SLIX poller

--- applications/main/nfc/plugins/logger/formatter/protocols/slix/slix_poller_data_formatter.c
+++ applications/main/nfc/plugins/logger/formatter/protocols/slix/slix_poller_data_formatter.c
@@ -5,6 +5,16 @@
 #include "../nfc_hal/nfc_hal_formatter.h"
 #include <nfc/protocols/slix/slix_poller_history_data.h>
 
+static const char* states[SlixPollerStateNum] = {
+    [SlixPollerStateIdle] = "Idle",
+    [SlixPollerStateGetNxpSysInfo] = "GetNxpSysInfo",
+    [SlixPollerStateReadSignature] = "Read Signature",
+    [SlixPollerStateCheckPrivacyPassword] = "Check Privacy Pwd",
+    [SlixPollerStatePrivacyUnlock] = "Privacy Unlock",
+    [SlixPollerStateReady] = "Ready",
+    [SlixPollerStateError] = "Error",
+};
+
 static void iso15693_3_poller_data_format(
     const NfcPacket* request,
     const NfcHistoryData* data,
@@ -14,9 +24,11 @@ static void iso15693_3_poller_data_format(
 
     const char* event_text = iso15693_3_poller_data_format_event_type(slix_data->event);
     const char* error_text = slix_format_error(slix_data->error);
+    const char* state_text = states[slix_data->state];
     const char* command_text = nfc_hal_data_format_nfc_command(slix_data->command);
 
-    furi_string_printf(output, "E=%s, Err=%s, C=%s", event_text, error_text, command_text);
+    furi_string_printf(
+        output, "E=%s, S=%s, Err=%s, C=%s", event_text, state_text, error_text, command_text);
 }
 
 const NfcProtocolFormatterBase slix_poller_data_formatter = {
