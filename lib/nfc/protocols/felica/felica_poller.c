b59549a375639746a03649de1a97ef6cbe913a7b
zinongli <131403964+zinongli@users.noreply.github.com>
2025-10-01 23:52:05 +0400

    poller read is only incomp if in Lite workflow

--- lib/nfc/protocols/felica/felica_poller.c
+++ lib/nfc/protocols/felica/felica_poller.c
@@ -434,8 +434,9 @@ NfcCommand felica_poller_state_handler_read_lite_blocks(FelicaPoller* instance) 
 NfcCommand felica_poller_state_handler_read_success(FelicaPoller* instance) {
     FURI_LOG_D(TAG, "Read Success");
 
-    if(!instance->auth.context.auth_status.internal ||
-       !instance->auth.context.auth_status.external) {
+    if((!instance->auth.context.auth_status.internal ||
+        !instance->auth.context.auth_status.external) &&
+       instance->data->workflow_type == FelicaLite) {
         instance->data->blocks_read--;
         instance->felica_event.type = FelicaPollerEventTypeIncomplete;
     } else {
